d68b71b4383dd009e78012dad220f9a9a7718c4e
jmanuelrc <juan.02324@gmail.com>
2018-10-07 12:51:48 -0500

    added function to glue all elements in a list

--- src/main/java/com/jmanuelrc/utils/ArrayUtils.java
+++ src/main/java/com/jmanuelrc/utils/ArrayUtils.java
@@ -1,5 +1,13 @@
 package com.jmanuelrc.utils;
 
+import java.util.ArrayList;
+
 public class ArrayUtils {
-	
+	public static String mImplode(ArrayList<String> pieces, String glue) {
+		String implode = "";
+		for (int i = 0; i < pieces.size(); i++) {
+			implode+=pieces.get(i) + glue;
+		}
+		return implode.substring(0, implode.length() - 1);
+	}
 }
